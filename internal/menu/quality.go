8ac451065e07dd642cffcea22801db4343e370d6
Rudolf Polzer <rpolzer@google.com>
2023-02-10 08:15:13 -0500

    Revert "Update to a 2.5 Ebitengine."
    This reverts commit ad3a2a5c3c3119682118c4a40edff306188d2411.
    
    This branch exists so one can run with Ebitengine 2.4 as needed.

--- internal/menu/quality.go
+++ internal/menu/quality.go
@@ -101,12 +101,12 @@ func (s qualitySetting) applyActual() error {
 		flag.Set("draw_blurs", true)
 		flag.Set("draw_outside", true) // <-
 		flag.Set("expand_using_vertices_accurately", true)
-		flag.Set("screen_filter", "linear2x")
+		flag.Set("screen_filter", "simple")
 	case mediumQuality:
 		flag.Set("draw_blurs", true) // <-
 		flag.Set("draw_outside", false)
 		flag.Set("expand_using_vertices_accurately", true)
-		flag.Set("screen_filter", "linear2x") // <-
+		flag.Set("screen_filter", "simple") // <-
 	case lowQuality:
 		flag.Set("draw_blurs", false)
 		flag.Set("draw_outside", false)
